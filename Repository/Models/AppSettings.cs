d517f327cb4ba931c3416c4eed831cf2ac7ea1fb
T180203-王鈞則 <ray.wang@kerrytj.com>
2022-05-02 18:46:31 +0800

    First Commit

---
+++ Repository/Models/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Models
+{
+    public class AppSettings
+    {
+        public AppSettings(IConfiguration configuration)
+        {
+            configuration.Bind(this);
+        }
+    }
+}
---
+++ RestfulApi/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Repository.Models;
+
+namespace RestfulApi
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config, IHostingEnvironment env)
+        {
+            _env = env;
+            _config = config;
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.{_env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+
+            _config = builder.Build();
+            _config.Bind(new AppSettings(_config));
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IApplicationLifetime appLifetime)
+        {
+            if (_env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseCors(builder =>
+            {
+                List<string> allow = new List<string> { "http://*" };
+
+                if (_env.IsDevelopment())
+                {
+                    allow.Add("http://localhost");
+                }
+
+                builder.WithOrigins(allow.ToArray())
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowedToAllowWildcardSubdomains();
+
+            });
+
+            app.UseHttpsRedirection();
+            appLifetime.DisposeAutofac();
+            app.UseMvc();
+        }
+    }
+}
