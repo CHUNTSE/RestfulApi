879a43221d17a3ee4f5539efd7bf2077b9196a4f
T180203-王鈞則 <ray.wang@kerrytj.com>
2022-05-04 18:41:17 +0800

    薪曾LogMiddleware

---
+++ RestfulApi/Attributes/MiddlewareConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using RestfulApi.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulApi.Attributes
+{
+    public static class MiddlewareConfig
+    {
+        public static IApplicationBuilder UseLogMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LogMiddleware>();
+        }
+    }
+}
---
+++ RestfulApi/Middleware/LogMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulApi.Middleware
+{
+    public class LogMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LogMiddleware(RequestDelegate next, ILogger<LogMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext Context)
+        {
+            try
+            {
+                Stream requestStream = Context.Response.Body;
+
+                _logger.LogTrace(ReadStream(requestStream));
+
+                await _next(Context);
+
+                Stream responseStream = Context.Response.Body;
+
+                _logger.LogTrace(ReadStream(responseStream));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex.Message);
+            }
+        }
+
+        private string ReadStream(Stream stream)
+        {
+            string readtext = new StreamReader(stream).ReadToEnd();
+
+            stream.Position = 0;
+
+            return readtext;
+        }
+    }
+}
